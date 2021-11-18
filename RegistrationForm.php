c36e8f8d189c21613aa06a640b0f40ce213cc2b7
jamyak <yuguredesu@gmail.com>
2021-11-18 14:35:57 -0500

    Account Page Alteration
    Account page now includes login inputs. PHP backend still in progress.

---
+++ RegistrationForm.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style tyle="text/css">
+h1 {
+	text-align: center;
+	}
+table {
+	text-align: center;
+	vertical-align: middle;
+	border: 2px solid black;
+	border-collapse: collapse;
+	margin: 20px auto;
+	font-family: Verdana, Helvetica, serif;
+	}
+table tr:nth-child(even) {
+	background-color: #ccc;
+	}
+table tr:first-child {
+	border-bottom: 2px solid black;
+	font-weight: bold;
+	}
+td {
+	padding: 5px 15px 5px 15px;
+	border: 1px solid black;
+	}
+</style>
+</head>
+<body>
+<?php
+@$dbConnect = new mysqli('localhost', 'root', '', 'eventplanner');
+if (mysqli_connect_errno()) {
+	echo ("<p>Error: Unable to connect to database.</p>" .
+			"<p>Error code $dbConnect->connect_errno: $dbConnect->connect_error. </p>");
+	exit;
+	}
+
+// get data from the input boxes 
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$password = $_POST['password'];	   
+$type = 'user';
+
+if (!$firstname || !$lastname || !$email || !$phone || !$password) {
+    echo "<p>You have not entered all the required information. </p>";
+    exit;
+}
+
+// add slashes if add and strip slashes default is not turned on
+// magic_quotes_gpc is off by default in XAMPP, add \ if value contains a quote
+if (!get_magic_quotes_gpc()){
+	$firstname = addslashes($firstname); 
+	$lastname = addslashes($lastname);
+	$email = addslashes($email);
+	$phone = addslashes($phone);
+	$password = addslashes($password);
+}
+
+// insert into contact database
+$sqlString = "INSERT into tbluser values " .
+				"(0, '$firstname', '$lastname', '$email', '$phone', '$password', '$type')";
+$result = $dbConnect->query($sqlString);
+if (!$result){	
+	echo ("<p>Error: Registration information was not added.</p>" .
+			"<p>Error code $dbConnect->errno: $dbConnect->error. </p>");
+	$dbConnect->close();
+	exit;
+	}
+
+$dbConnect->close();
+//** end of input processing
+?>
+<div id=header>
+	<h1>Thank You for Registering</h1>
+</div>
+<table>
+	<tr>
+		<td>User Id</td>
+		<td>First Name</td>
+		<td>Last Name</td>
+		<td>Email</td>
+		<td>Phone</td>
+		<td>Type</td>
+	</tr>	
+
+<?php 
+@$dbConnect = new mysqli('localhost', 'root', '', 'eventplanner');
+if (mysqli_connect_errno()) {
+	echo ("<p>Error: Unable to connect to database.</p>" .
+			"<p>Error code $dbConnect->connect_errno: $dbConnect->connect_error. </p>");
+	exit;
+	}
+$data = mysqli_query(@$dbConnect, "SELECT * FROM tbluser where firstname = '$firstname' && email = '$email' && password = '$password'") 
+ or die("Unable to select data"); 
+ #$info = mysqli_fetch_array($data);
+
+ while($info = mysqli_fetch_array( $data )) 
+ { 
+ echo "<tr>";  
+ echo "<td>" .$info['userid'] . "</td>";
+ echo "<td>".$info['firstname'] . " </td>";
+ echo "<td>".$info['lastname']. " </td>";
+ echo "<td>" .$info['email']. " </td>";
+ echo "<td>" .$info['phone']. " </td>";
+ echo "<td>" .$info['type']. " </td>";
+ echo "</tr>";
+ } 
+
+
+include'eventregistration.html'
+?>
+</body>
+</html>
